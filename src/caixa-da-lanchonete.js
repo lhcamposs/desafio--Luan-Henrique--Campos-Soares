1fa4d0e54b1be4b8747dc9cbc327f478e32508de
lhcamposs <lhcampossoares@gmail.com>
2023-08-15 21:23:24 -0300

    Segundo commit

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,22 @@
+
+
+const descontoDinheiro = 0.05;
+const acrescimoCredito = 0.03;
+
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+
+
+        if (metodoDePagamento === "dinheiro"){
+            total -= total * this.descontoDinheiro;
+        } else if(metodoDePagamento === "credito"){
+            total += total * this.acrescimoCredito;
+        } else if(!["debito", "credito", "dinheiro"].includes(metodoDePagamento)){
+            return "Forma de pagamento invalida! Favor tentar novamente.";
+        }
+
+        
         return "";
     }
 
